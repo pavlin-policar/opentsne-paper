4ba4ef329f4b23197ff07f2f797026b5b51206a4
Pavlin Poličar <pavlin.g.p@gmail.com>
2021-11-15 11:53:07 +0000

    Add benchmark setup and run script

---
+++ benchmarks/run.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -e
+
+#conda create --name tsne -y
+#conda activate tsne
+#conda install -y python numpy scikit-learn
+
+#pip install opentsne
+#pip install -r requirements-benchmarks.txt
+
+# Setup FIt-SNE
+git clone git@github.com:KlugerLab/FIt-SNE.git || :
+conda install -c conda-forge fftw 
+cd FIt-SNE
+CPLUS_INCLUDE_PATH="${CONDA_PREFIX}/include/" LIBRARY_PATH="${CONDA_PREFIX}/lib/" \
+    g++ -std=c++11 -O3  src/sptree.cpp src/tsne.cpp src/nbodyfft.cpp  \
+    -o bin/fast_tsne -pthread -lfftw3 -lm -Wno-address-of-packed-member
+cd ..
+
+# Fetch bencmarking module from openTSNE
+wget -nc https://raw.githubusercontent.com/pavlin-policar/openTSNE/master/benchmarks/benchmark.py
+# Download benchmark dataset
+mkdir -p data
+wget -nc -P data http://file.biolab.si/opentsne/10x_mouse_zheng.pkl.gz
+
+# Prepare logs directory
+mkdir -p logs
+
+# RUN BENCHMARS
+SAMPLE_SIZES=(1000 100000 250000 500000 750000 1000000);
+REPETITIONS=6;
+
+# Single-threaded benchmarks
+#METHODS=(openTSNEBH openTSNEFFT MulticoreTSNE FItSNE sklearn UMAP);
+METHODS=(MulticoreTSNE FItSNE UMAP);
+
+for method in ${METHODS[@]}; do
+    for size in ${SAMPLE_SIZES[@]}; do
+        cmd="OMP_NUM_THREADS=1 NUMBA_NUM_THREADS=1 \
+            python benchmark.py $method run_multiple \
+            --n $REPETITIONS \
+            --n-samples $size 2>&1 \
+            | tee -a logs/${method}_${size}.log";
+        echo "$cmd";
+        eval "$cmd";
+    done;
+done;
+
+# Multi-threaded benchmarks
+#METHODS=(openTSNEBH_8core openTSNEFFT_8core MulticoreTSNE_8core FItSNE_8core sklearn_8core UMAP_8core);
+#METHODS=(MulticoreTSNE_8core FItSNE_8core sklearn_8core);
+METHODS=(MulticoreTSNE_8core FItSNE_8core sklearn_8core UMAP_8core);
+
+for method in ${METHODS[@]}; do
+    for size in ${SAMPLE_SIZES[@]}; do
+        cmd="OMP_NUM_THREADS=8 NUMBA_NUM_THREADS=8 \
+            python benchmark.py $method run_multiple \
+            --n $REPETITIONS \
+            --n-samples $size 2>&1 \
+            | tee -a logs/${method}_${size}.log";
+        echo "$cmd";
+        eval "$cmd";
+    done;
+done;
