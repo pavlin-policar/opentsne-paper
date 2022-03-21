97a46e7065bada2d9415090725fe9d69abec6278
Pavlin Poličar <pavlin.g.p@gmail.com>
2022-03-21 08:53:04 +0000

    Figs: Add bash script to generate all figures

---
+++ figures/generate_figures.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+H5AD_DIR="./data/h5ad"
+FIGURES_DIR="./figs"
+
+
+cat << EOF
+================================================================================
+Installing Python dependencies...
+================================================================================
+EOF
+
+pip install -r requirements-figures.txt
+
+
+cat << EOF
+================================================================================
+Generating figures...
+================================================================================
+EOF
+
+mkdir -p "$FIGURES_DIR"
+
+echo "Generating Figure 1..."
+python fig1-macosko.py -i "$H5AD_DIR" -o "$FIGURES_DIR"
